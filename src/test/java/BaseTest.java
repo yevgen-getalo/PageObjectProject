ae0f6b8ed6871d27c3487e4a78e990467bab7ba4
Yevgen <yevgen.getalo@gmail.com>
2017-05-22 16:12:52 +0300

    added new assertion

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,12 +3,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
     WebDriver driver;
 
     @BeforeTest
     public void beforeTest() {
         driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS).setScriptTimeout(20, TimeUnit.SECONDS);
         driver.manage().window().maximize();
     }
 
